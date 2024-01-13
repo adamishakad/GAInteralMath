571826efb009651055a4b783c8607dd90df5d8fd
adamishakad <adamishag1@gmail.com>
2024-01-12 16:46:37 -0800

    Updated Program File

--- Program.cs
+++ Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Exploring C# Integral Types and Math Operations");
+            // additional implementation will follow
+            int myInt = 2147483647; 
+            //Int32: Ranges from -2,147,483,648 to 2,147,647. Commonly used for genera.
+            //long myLong = 9223372036854775887L;
         }
     }
 }
